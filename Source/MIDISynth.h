4cd3867d42fce78caf06426b12bd70a610c72c25
DM Welch <dmw9400@gmail.com.com>
2024-11-11 22:10:45 -0500

    classes defined

--- Source/MIDISynth.h
+++ Source/MIDISynth.h
@@ -9,3 +9,54 @@
 */
 
 #pragma once
+#include <JuceHeader.h>
+
+class Tone
+{
+public:
+    enum WaveType {Sine, Squre, Sawtooth};
+    
+    Tone(float frequency, float velocity, WaveType waveType, double sampleRate);
+    ~Tone();
+    
+    void setSampleRate(double newSampleRate);
+    void setWaveType(WaveType newWaveType);
+    void setFrequency(double newFrequency);
+    void setGain (double newGain);
+    void setReleased();
+    void updateCounter();
+    void updateTone();
+    void processSample(float& sample);
+    bool shouldBeRemoved();
+    
+private:
+    WaveType waveType;
+    double frequency;
+    bool isReleased = false;
+    double gain, velocity;
+    long long counter;
+    double sampleRate;
+    
+    float generateWaveSample();
+    
+};
+
+class ToneBank 
+{
+public:
+    ToneBank();
+    ~ToneBank();
+    
+    void prepareToPlay(double newSampleRate);
+    void setWaveType(Tone::WaveType waveType);
+    void noteOn(float frequency, float velocity, Tone::WaveType wavetype);
+    void noteOff(float frequency);
+    void renderBuffer(juce::AudioBuffer<float>& buffer);
+    
+private:
+    std::vector<std::unique_ptr<Tone>> tones;
+    Tone::WaveType wavetype;
+    double sampleRate;
+    double ATTACK_FACTOR, DECAY_FACTOR;
+};
+
